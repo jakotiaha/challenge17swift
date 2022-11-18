4bd3d6ee807bd4c14e2bef09d6484cef9379298e
Akshat Jakotiah <jakotiah.a@etoncollege.org.uk>
2022-11-18 16:56:25 +0000

    Initial Commit

---
+++ challenge17swift/challenge17swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge17swiftApp.swift
+//  challenge17swift
+//
+//  Created by Jakotiah, Akshat (PGW) on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct challenge17swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
